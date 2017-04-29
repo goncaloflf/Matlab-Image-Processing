1d98ce6dd10626eeb084d563780f421801c7a01f
Afonso Faria <afonsofaria10@gmail.com>
2017-04-29 14:09:26 +0100

    fixed false positives

--- proj1_try.m
+++ proj1_try.m
@@ -17,7 +17,7 @@ figure;
 
 
 for i = 1: seqLength
-
+    i
     imgfr = imread(sprintf('SonMated\\frame_%.1d.tif',i)); %corre cada frame do video com o ciclo //works
     hold off
     imshow(imgfr);
@@ -39,6 +39,10 @@ for i = 1: seqLength
     regionProps = regionprops(lb,'area','Filledimage','Centroid'); % vai buscar as propriedades das zonas encontradas //works
     inds = find([regionProps.Area]>minArea);
     
+%     for z=1:length(regionProps)
+%        regionProps.Area 
+%     end
+    
     
     regnum = length(inds);  
     
@@ -48,23 +52,30 @@ for i = 1: seqLength
             upLPoint = min([lin col]);
             dWindow = max([lin col]) - upLPoint + 1;
             
-%             cent1(i)= regionProps(j).Centroid(1);
-%             cent2(i)= regionProps(j).Centroid(2);
-%            
-%              TENTATIVA DE FAZER O PATH, QUASE QUE FUNCIONAVA 
+            
+%             centmin = 1;
+%             cent1(i)= regionProps(inds(j)).Centroid(1);
+%             cent2(i)= regionProps(inds(j)).Centroid(2); 
 % 
-%             for l=1:length(cent1)
+%             if(length(cent1) > 10)
+%                 centmin = (length(cent1)-10);
+%             end
+%             
+%             for l=(centmin):length(cent1)
 %                 hold on; axis off;
+%                 %line([cent1(l-1) cent2(l-1)] , [cent1(l) cent2(l)], [1 1], 'Marker','.','LineStyle','-', 'Color','red');
 %                 plot(cent1(l),cent2(l), 'Marker', 'd','MarkerFaceColor' ,'r', 'MarkerEdgeColor' ,'k','MarkerSize',3 );
 %             end
-                
+              
+            
+            
             rectangle('Position',[fliplr(upLPoint) fliplr(dWindow)], 'EdgeColor',[1 1 0], 'linewidth',2);
         end
 
-    if (length(regionProps) == 2)
+    if (length(inds) > 1)
         
-        point1=[regionProps(1).Centroid(1), regionProps(1).Centroid(2)];
-        point2=[regionProps(2).Centroid(1), regionProps(2).Centroid(2)];
+        point1=[regionProps(inds(1)).Centroid(1), regionProps(inds(1)).Centroid(2)];
+        point2=[regionProps(inds(2)).Centroid(1), regionProps(inds(2)).Centroid(2)];
 
         X = [point1(1), point1(2); point2(1), point2(2)];    
 
@@ -72,22 +83,7 @@ for i = 1: seqLength
         distance = pdist(X,'euclidean');
         line([point1(1) point2(1)] , [point1(2) point2(2)], [1 1], 'Marker','.','LineStyle','-', 'Color','red');
         middlePoint = [(point1(1)+point2(1))/2, (point1(2)+point2(2))/2];
-        t = text(middlePoint(1), middlePoint(2), num2str(distance));    
-         
-    elseif (length(regionProps) > 2)
-%         Amax=0;
-%         Amin=0;
-%         for k=1:length(regionProps)
-%             if regionProps(k).Area > Amax
-%                 Amax = regionProps(k).Area;
-%                 point1=[regionProps(k).Centroid(1), regionProps(k).Centroid(2)];
-%             elseif (regionProps(k).Area < Amax && regionProps(k).Area > Amin)
-%                 Amin = regionProps(k).Area;
-%                 point2=[regionProps(k).Centroid(1), regionProps(k).Centroid(2)];
-%             end
-%         end
-     
- 
+        t = text(middlePoint(1), middlePoint(2), num2str(distance));            
         
     else
         distance = 0;
@@ -101,7 +97,7 @@ for i = 1: seqLength
     plot(x,y);
     drawnow
     subplot(1,2,2);
-    
+
    
     end
 
