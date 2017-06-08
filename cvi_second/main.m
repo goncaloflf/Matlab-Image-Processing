233f4341e224ad300a623bf105e2bdd4974e3d81
Afonso Faria <afonsofaria10@gmail.com>
2017-06-08 03:04:53 +0100

    iou

--- cvi_second/main.m
+++ cvi_second/main.m
@@ -2,6 +2,8 @@ clear all, close all
 warning('off', 'Images:initSize:adjustingMag');
 seqLength = 1000;
 time_struct = [];
+regions_vector = {};
+truth_vector = [];
 
 %Load Ground Truth into array vectorGT   [frameNr x y w h id tf]  id e tf
 %são irreleventes
@@ -26,7 +28,22 @@ for i = 0: 5:  seqLength
     end
     [mask_v, targets]= vessel_detection(imgfr,9,200,1);
     [ region,time_struct ] = target_filter(targets, time_struct, imgfr, 2809+i);
-
+    
+    if (~isequal(region, []))
+        regions_vector(i).Area = region.Area;
+        regions_vector(i).Centroid = region.Centroid;
+        regions_vector(i).BoundingBox = region.BoundingBox;
+        regions_vector(i).FilledImage = region.FilledImage;
+        indice = find (vectorGT(:,1) == 2809 + i);
+        size = length(truth_vector)+1;
+        truth_vector(size).Frames = vectorGT(indice,1);
+        truth_vector(size).X = vectorGT(indice,2);
+        truth_vector(size).Y = vectorGT(indice,3);
+        truth_vector(size).W = vectorGT(indice,4);
+        truth_vector(size).H = vectorGT(indice,5);
+        
+    end
+    
     %imshow(imgdif);
     %drawnow
     
